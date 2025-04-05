6e6a38c228e9a45cb88386f8786c032105c11385
James Barron <jjbarron0810@gmail.com>
2025-04-05 20:44:19 +0100

    fix the portal link

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -74,12 +74,12 @@ const showingNavigationDropdown = ref(false);
                                         >
                                             Profile
                                         </DropdownLink>
-                                        <a v-if="$page.props.auth.user?.subscription_active" :href="$page.props.stripePortal">
+                                        <a v-if="$page.props.subscription_active" :href="$page.props.stripePortal">
                                             <button class="block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out">
                                                 Billing &amp; Upgrades
                                             </button>
                                         </a>
-                                        <a v-if="!$page.props.auth.user?.subscription_active" :href="route('site.plans')">
+                                        <a v-if="!$page.props.subscription_active" :href="route('site.plans')">
                                             <button class="block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out">
                                                 Plans
                                             </button>
